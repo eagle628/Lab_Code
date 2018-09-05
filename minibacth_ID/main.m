42bf57e45817975985604f906bc6c8120aa0b602
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-09-05 19:04:08 +0900

    'various'

--- minibacth_ID/main.m
+++ minibacth_ID/main.m
@@ -1,10 +1,9 @@
 clear 
 close all
-addpath('C:\Users\Naoya Inoue\Documents\GitHub\Lab_Code\sysID_adam_k\systemID')
 s = tf('s');
 rng(100)
 Ts = 0.1;
-sys = 1/(s+1);
+sys = (s+5)/(s+1)/(s+10);
 K = 1/(s+2);
 
 sys_all = 1/(1+sys*K);
@@ -16,12 +15,24 @@ Gw = K/(1+sys*K);
 
 Gwr = 1/(1+sys*K);
 Gvr = sys/(1+sys*K);
+%% Plant character
+min = -6;
+max =  6;
+[mag_env, pha_env, omega] = bode(sys, {10^min, 10^max});
+mag_env = squeeze(mag_env(1,1,:));
+pha_env = squeeze(pha_env(1,1,:));
+%% minibatch
+maxitr = 4;
+state = 2;
+in = 1;
+out = 1;
+% get memory
+Theta1 = zeros(maxitr, 2*state+1);
+Mag = zeros(length(omega), maxitr);
+Pha = zeros(length(omega), maxitr);
 
-maxitr = 1;
-Theta1 = zeros(maxitr, 2);
-Theta2 = zeros(maxitr, 2);
 parfor_progress(maxitr);
-for itr = 1:maxitr
+parfor itr = 1:maxitr
 
 N = 1000;
 d = lsim(H, randn(N,1)*1);
@@ -31,17 +42,31 @@ t = (0:N-1)'*Ts;
 v = lsim(Gv, d, t) + lsim(Gvr, r, t);
 w = lsim(Gw, d, t) + lsim(Gwr, r, t);
 
-m = model_ss(gen_ss_rectifier(gen_ss_tridiag(1, 1, 1), K));
+m = model_ss(gen_ss_rectifier(gen_ss_tridiag(state, in, out), K));
 m.add_fixed_params('theta_D_1', 0); % model.m
 % m.str_display='none';
-m.max_iter = 1e-4;
+m.max_iter = 1e4;
 % m.fit(t, [w, v], zeros(N, 1));
 m.fit_adam(t, [w, v], zeros(N, 1), 1e-5);
-model1 = tf(m.gen_ss.gen_ss.get_sys());
-Theta1(itr, :) = [model1.Denominator{1}(2), model1.Numerator{1}(2)];
+%   eval_func内部の離散化は，あくまで，シミュレーション用であるので，
+%   出てくるパラメータ自体は，連続時間のもの
+model1 = m.gen_ss.gen_ss.get_sys();
+[num, den] = tfdata(model1,'v');
+Theta1(itr, :) = [den(2:end), num];
+% model character
+[mag, pha] = bode(model1, omega);
+mag = squeeze(mag(1,1,:));
+pha = squeeze(pha(1,1,:));
+Mag(:, itr) = mag;
+Pha(:, itr) = pha;
 
 parfor_progress();
 end
 parfor_progress(0);
 
+%% Drawing
+H = figure_config.set_figure_bode();
+figure_config.plot_bode(H.axes,omega,mag_env,pha_env,{'r:','linewidth',3.0});
+
+figure_config.plot_bode(H.axes,omega,Mag,Pha,{'b-','linewidth',0.8});
 
