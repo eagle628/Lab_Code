9d1f10bf81a497ecd33bc350d1950ffcc8182866
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-09-05 20:24:46 +0900

    'various'

--- minibacth_ID/main_minibacth.m
+++ minibacth_ID/main_minibacth.m
@@ -6,16 +6,18 @@ Ts = 0.1;
 sys = tf([-1.0860,-0.0653,-2.6176,-0.0829,-0.0000],...
             [1.0000,0.0601,3.0582,0.0893,0.9075]);
 K = tf([0,0,0.6448],[1,0.0494,0]);
+% sys = (s+4)/(s+1)/(s+1);
+% K = 1/(s+2);
 
 sys_all = 1/(1+sys*K);
 
 H = tf(1, 1, Ts);
 
-Gv = 1/(1+sys*K);
-Gw = K/(1+sys*K);
+Gvd = 1/(1-sys*K);
+Gwd = K/(1-sys*K);
 
-Gwr = 1/(1+sys*K);
-Gvr = sys/(1+sys*K);
+Gwr = 1/(1-sys*K);
+Gvr = sys/(1-sys*K);
 %% Plant character
 min = -6;
 max =  6;
@@ -23,8 +25,8 @@ max =  6;
 mag_env = squeeze(mag_env(1,1,:));
 pha_env = squeeze(pha_env(1,1,:));
 %% minibatch
-maxitr = 4;
-state = 2;
+maxitr = 1;
+state = 4;
 in = 1;
 out = 1;
 % get memory
@@ -33,15 +35,15 @@ Mag = zeros(length(omega), maxitr);
 Pha = zeros(length(omega), maxitr);
 
 parfor_progress(maxitr);
-parfor itr = 1:maxitr
-
+for itr = 1:maxitr
+rng('shuffle')
 N = 1000;
 d = lsim(H, randn(N,1)*1);
 r = randn(N, 1)*1;
 t = (0:N-1)'*Ts;
 
-v = lsim(Gv, d, t) + lsim(Gvr, r, t);
-w = lsim(Gw, d, t) + lsim(Gwr, r, t);
+v = lsim(Gvd, d, t) + lsim(Gvr, r, t);
+w = lsim(Gwd, d, t) + lsim(Gwr, r, t);
 
 m = model_ss(gen_ss_rectifier(gen_ss_tridiag(state, in, out), K));
 m.add_fixed_params('theta_D_1', 0); % model.m
