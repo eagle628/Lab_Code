2e7de72a99f81426b7298a987f02d59ee4d4d91d
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-09-07 19:58:40 +0900

    'smilogy'

--- minibatch_ID/callback/callback_sgd.m
+++ minibatch_ID/callback/callback_sgd.m
@@ -3,7 +3,7 @@ function callback_sgd(x, v,  itr, y, yhat)
 fprintf('%d, %e, %e\t', itr, v, x);
 fprintf('\n');
 opt.iteration = itr;
-subplot(3,1,3), plot(itr, v, 'r*', 'MarkerSize', 5), hold on
+subplot(3,1,3), semilogy(itr, v, 'r*', 'MarkerSize', 5), hold on
 f = gcf;
 if mod(numel(f.CurrentAxes.Children),100) == 0
     delete(f.CurrentAxes.Children(end-9:end));
