a3f77330cdcafadbca0254882e9f866e077296a4
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-09-07 15:27:11 +0900

    'Sa'

--- minibacth_ID/callback/callback_sgd.m
+++ minibacth_ID/callback/callback_sgd.m
@@ -5,8 +5,8 @@ fprintf('\n');
 opt.iteration = itr;
 subplot(3,1,3), plot(itr, v, 'r*', 'MarkerSize', 5), hold on
 f = gcf;
-if numel(f.CurrentAxes.Children) > 100
-    delete(f.CurrentAxes.Children(end));
+if mod(numel(f.CurrentAxes.Children),100) == 0
+    delete(f.CurrentAxes.Children(end-9:end));
 end
 
 callback_fmin(x, opt, [], y, yhat, []);
