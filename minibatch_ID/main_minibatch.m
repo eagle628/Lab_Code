f00956144eb6742d06895894895ada9bb99b13e9
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-09-14 19:15:16 +0900

    'corr_various_optimizer'

--- minibatch_ID/main_minibatch.m
+++ minibatch_ID/main_minibatch.m
@@ -2,7 +2,7 @@ clear
 close all
 %% Generate Network
 seed = 3;
-Node_number = 2;
+Node_number = 3;
 n_ori = network_swing_simple(Node_number, [1,2], [2,10]*1e-2, 1, [1,5], 0.1, seed);
 n_ori.Adj_ref = n_ori.Adj_ref*0;
 n_ori.plot()
@@ -11,10 +11,11 @@ c_n = 1;
 n_n = [2];
 %% signal power
 id_in_p = 1;
-noise_p = 0;
+noise_p = 0.1;
 %% Node Name
 ob_v  = {strcat('v_node',num2str(c_n))};
 ob_w  = {strcat('w_node',num2str(c_n))};
+ob_y  = {strcat('y_node',num2str(c_n))};
 ID_in = {strcat('d_node',num2str(c_n))};
 Noise = cell(1,numel(n_n));
 for i = 1 : numel(n_n)
@@ -36,7 +37,7 @@ pha_env = squeeze(pha_env(1,1,:));
 %% add controller
 % Q = kron(eye(1*numel(1)),diag([1,1000]));
 % R = kron(eye(1*numel(1)),diag([1e-3]));
-% n_ori.add_controller( 2, Q, R);
+% n_ori.add_controller( c_n, Q, R);
 % sys_ori = n_ori.get_sys_controlled(sys_ori);
 %% Generate v & w
 N = 10000;
@@ -58,42 +59,65 @@ parfor_progress(maxitr);
 
 % init_params = 1e-3;
 
-%
 for itr = 1:maxitr
     d = randn(N,2+numel(n_n)*2);
     d(:,1:2) = (d(:,1:2)+0.5)*id_in_p;
     d(:,3:end) = d(:,3:end)*noise_p;
     % Response of v&w 
-    rng(28);
+%     rng(28);
+    rng('shuffle')
     v = lsim(sys_ori(ob_v, cat(2,ID_in,Noise)), d, t);
     w = lsim(sys_ori(ob_w, cat(2,ID_in,Noise)), d, t);
 
-    m = model_ss(gen_ss_rectifier(gen_ss_tridiag(state, in, out), sys_local_vw));
-%     m = model_ss(gen_ss_rectifier(gen_ss_all(state, in, out), sys_local_vw));
+%     m = model_ss(gen_ss_rectifier(gen_ss_tridiag(state, in, out), sys_local_vw));
+    m = model_ss(gen_ss_rectifier(gen_ss_all(state, in, out), sys_local_vw));
 %     m = model_ss(gen_ss_rectifier(gen_ss_canonical(state, in, out), sys_local_vw));
-    m.max_iter = 5e3;
     %%%%%%%%%%%%%%%%%%%%%
-    best_cost = m.eval_func(t, [w, v], zeros(N, 1), sys2params_tri(sys_env, state, in, out));
-%     best_cost = m.eval_func(t, [w, v], zeros(N, 1), sys2params_all(sys_env, state, in, out));
+%     best_cost = m.eval_func(t, [w, v], zeros(N, 2), sys2params_tri(sys_env, state, in, out));
+    best_cost = m.eval_func(t, [w, v], zeros(N, 1), sys2params_all(sys_env, state, in, out));
 %     best_cost = m.eval_func(t, [w, v], zeros(N, 1), sys2params_canonical(sys_env, state, in, out));
     fprintf('Best Cost is %e.', best_cost);
     fprintf('\n');
     %%%%%%%%%%%%%%%%%%%%%
     % init
+%     init_sys = sys_env;
     init_sys = d2c(arx(iddata(v,w,Ts), [state, state+1, 0])); 
-    init_params = sys2params_tri(init_sys, state, in, out); 
+%     init_sys = d2c(armax(iddata(v,w,Ts), [state, state+1, state, 0])); 
+%     init_sys = d2c(oe(iddata(v,w,Ts), [state+1, state, 0])); 
+%     init_params = sys2params_tri(init_sys, state, in, out); 
 %     init_params = ones(3*state-2+state*(in+out)+in*out, 1)*1e-3;
 %     m.add_fixed_params('theta_A_6', 0); % model.m
 %     m.add_fixed_params('theta_A_9', 0); % model.m
 %     init_params = nonzeros(init_params);
-%     init_params = sys2params_canonical(ss(d2c((init_sys))), state, in, out);
+%     init_params = sys2params_canonical(ss(init_sys), state, in, out);
 %     init_sys = rss(state);
-%     init_params = sys2params_all(ss(d2c(init_sys)), state, in, out);
+    init_params = sys2params_all(ss(init_sys), state, in, out);
 %     init_params = sys2params_all(init_sys, state, in, out);
     
+
+%     m.add_fixed_params('theta_C_1', 0);
+%     init_params(state+1) = [];
+%%%%%%%%%%%%%%% fitting
+    m.max_iter = 5000;
     % optimizer
-    [theta ,J] = m.fit_adam(t, [w, v], zeros(N, 1), init_params, 1e-5, 0.9, 0.999, [], 0.5, 1);
-%     [theta ,J] = m.fit_adamax(t, [w, v], zeros(N, 1), init_params, [], [], [], 0.6);
+    rng('shuffle')
+    load('params.mat');
+%     theta= init_params;
+%     init_params = ones(3*state-2+state*(in+out)+in*out, 1)*randn(1)*1e-4;init_params(end) = 1e-3;
+    for itr1 = 1: 10
+        [theta ,J] = m.fit_adam(t, [w, v], zeros(N, 1), theta, 1e-5, 0.9, 0.999, 1e-4, 0.01, 0);
+        model1 = m.gen_ss.gen_ss.get_sys();
+        bode(sys_env,init_sys,model1)
+    end
+%     [re_theta ,J] = m.fit_adamax(t, [w, v], zeros(N, 1), init_params, [], [], [], 0.8);
+%     theta = 1e-4;
+%     theta(end) = 1e-3;
+%     theta = init_params;
+%     for itr1 = 1 : 2
+%         [theta,J] = m.fit_Santa_S(t, [w, v], zeros(N, 1), theta, 1e-7, [], 1e-1, 2000, @(t)t^2, 0.01);
+%         model1 = m.gen_ss.gen_ss.get_sys();
+%         bode(sys_env,init_sys,model1)
+%     end
     %   eval_func内部の離散化は，あくまで，シミュレーション用であるので，
     %   出てくるパラメータ自体は，連続時間のもの
     model1 = m.gen_ss.gen_ss.get_sys();
@@ -111,18 +135,33 @@ end
 parfor_progress(0);
 
 %% Drawing
-H = figure_config.set_figure_bode();
-figure_config.plot_bode(H.axes,omega,mag_env,pha_env,{'r:','linewidth',3.0});
-    
-    [mag, pha] = bode(init_sys, omega);
-    mag = squeeze(mag(1,1,:));
-    pha = squeeze(pha(1,1,:));
-figure_config.plot_bode(H.axes,omega,mag,pha,{'g:','linewidth',2.0});
+% H = figure_config.set_figure_bode();
+% figure_config.plot_bode(H.axes,omega,mag_env,pha_env,{'r:','linewidth',3.0});
+%     
+%     [mag, pha] = bode(init_sys, omega);
+%     mag = squeeze(mag(1,1,:));
+%     pha = squeeze(pha(1,1,:));
+% figure_config.plot_bode(H.axes,omega,mag,pha,{'g:','linewidth',2.0});
+% 
+% figure_config.plot_bode(H.axes,omega,Mag,Pha,{'b-','linewidth',0.8});
+figure
+bode(sys_env,init_sys,model1)
+legend('original','Init','identification')
 
-figure_config.plot_bode(H.axes,omega,Mag,Pha,{'b-','linewidth',0.8});
-    
 figure('Name','Cost History')
 semilogy(J)
+
+%% simlation Extend
+%% add controller
+Q = kron(eye(1*numel(1)),diag([1,1000]));
+R = kron(eye(1*numel(1)),diag([1e-3]));
+n_ori.controllers = {};
+n_ori.add_controller( c_n, model1, Q, R);
+sys_ori = n_ori.get_sys_controlled(sys_ori);
+y = impulse(sys_ori(ob_y, ID_in), t);
+figure
+plot(t,y(:,:,1))
+
 
 %% local function
 function init_params = sys2params_tri(init_sys, state, in, out)
@@ -154,4 +193,4 @@ function init_params = sys2params_canonical(init_sys, state, in, out)
 end
 
 %%
-%     m.add_fixed_params('theta_D_1', 0); % model.m
+% m.add_fixed_params('theta_D_1', 0);
