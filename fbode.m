b764348b018353a81f5cad3922dec3af8bd840b2
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-12-04 15:32:41 +0900

    add fucntion note

--- fbode.m
+++ fbode.m
@@ -1,3 +1,13 @@
+%% NOTE
+% % Calculating Bode fucnttion of Continuous sytem
+% % gain and angle are calculating function-handle
+% % So, You need to "fplot" drawing bode plot.
+% % EX)
+% %    [MAG,PHASE] = fbode(sys);
+% %    fplot(@(w)mag2db(MAG{1}(w)),[wmin, wmax])
+% %    fplot(@(w)PHASE{1}(w),[wmin, wmax])
+% % Only Minimum Phas system
+%% main
 function [gain_func, ang_func] = fbode(sys)
     if sys.Ts ~= 0
         sys = d2c(sys,'foh');
@@ -13,7 +23,7 @@ function [gain_func, ang_func] = fbode(sys)
             den = den_set{itr1, itr2};
             G = @(w) polyval(num, 1i*w)./polyval(den, 1i*w);
             gain_func{itr1, itr2} = @(w) abs(G(w));
-            ang_func{itr1, itr2}  = @(w) angle(G(w));
+            ang_func{itr1, itr2}  = @(w) rad2deg(angle(G(w)));
         end
     end
 end
