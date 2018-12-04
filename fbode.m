d5fb704a5a7e4d6cf5434a4774eb128c9e12a720
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2018-12-04 15:22:28 +0900

    add fbode function

--- fbode.m
+++ fbode.m
@@ -5,14 +5,13 @@ function [gain_func, ang_func] = fbode(sys)
     [num_set, den_set] = tfdata(sys);
     [no, ni] = size(num_set);
 
-    j = sqrt(-1);
     gain_func = cell(no, ni);
     ang_func  = cell(no, ni);
     for itr1 = 1 : no
         for itr2 = 1 : ni
             num = num_set{itr1, itr2};
             den = den_set{itr1, itr2};
-            G = @(w) polyval(num, j*w)/polyval(den, j*w);
+            G = @(w) polyval(num, 1i*w)./polyval(den, 1i*w);
             gain_func{itr1, itr2} = @(w) abs(G(w));
             ang_func{itr1, itr2}  = @(w) angle(G(w));
         end
