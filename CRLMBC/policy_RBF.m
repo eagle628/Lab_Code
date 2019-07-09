8c8751538071de1da21f98ee90e844eee18a5f66
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2019-07-09 13:28:55 +0900

    fix sigma

--- CRLMBC/policy_RBF.m
+++ CRLMBC/policy_RBF.m
@@ -18,7 +18,7 @@ classdef policy_RBF < approximate_function_class
             input = obj.apx_function.basis_func(state)'*theta;
 %             input = arrayfun(@obj.apx_function.basis_func, gpuArray(single(state)), gpuArray(single(theta)));
 %             input = gather(input);
-            input = input + obj.sigma*randn(size(input));
+            input = input + obj.sigma^2*randn(size(input));
         end
         
         function input = determistic_policy(obj, state, theta)
@@ -26,7 +26,7 @@ classdef policy_RBF < approximate_function_class
         end
         
         function grad = policy_grad(obj, pre_input, state, theta)
-            grad = ((pre_input - obj.apx_function.basis_func(state)'*theta)./(obj.sigma))*obj.apx_function.basis_func(state);
+            grad = ((pre_input - obj.apx_function.basis_func(state)'*theta)./(obj.sigma^2))*obj.apx_function.basis_func(state);
 %             tmp1 = arrayfun(@obj.determistic_policy, gpuArray(single(state)), gpuArray(single(theta)));
 %             tmp2 = arrayfun(@obj.apx_function.basis_func, gpuArray(single(state)), gpuArray(single(theta)));
 %             grad = ((pre_input - gather(tmp1))./(obj.sigma)^2)*gather(tmp2);
