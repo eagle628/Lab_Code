3379c0ca74cbd6aa4cc0fe9c995638f5c146ddcd
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2019-09-02 15:20:42 +0900

    add dynamic policy(LTI) init

---
+++ RL/policy_dynamic.m
@@ -0,0 +1,93 @@
+classdef policy_dynamic < approximate_function_class
+    %UNTITLED11 このクラスの概要をここに記述
+    %   詳細説明をここに記述
+    
+    properties
+        sigma
+        internal_state
+        internal_state_ % previous one step state
+        grad_internal_state
+        grad_internal_state_
+    end
+    
+    methods
+        function obj = policy_RBF(gen_ss_tridiag, sigma)
+            obj.apx_function = gen_ss_tridiag;
+            obj.sigma = sigma;
+            obj.params = obj.apx_function.get_params();
+            obj.internal_state  = zeros(obj.apx_fucntion.n, 1);
+            obj.internal_state_ = zeros(obj.apx_fucntion.n, 1);
+            obj.grad_internal_state  = zeros(obj.apx_function.n*length(obj.params));
+            obj.grad_internal_state_ = zeros(obj.apx_function.n*length(obj.params));
+        end
+        
+        function input = stocastic_policy(obj, state, varargin)
+            [a,b,c,d] = obj.apx_function.get_ss(obj.params);
+            ABCD = [a,b;c,d];
+            xy = ABCD*[obj.internal_state;state];
+            input = xy(size(a, 1)+1 : end);
+            obj.internal_state_ = obj.internal_state;
+            obj.internal_state  = xy(1 : size(a, 1));
+%             tmp = strcmp(varargin, 'Input-Clipping');
+%             if sum(tmp)
+%                 input(input>=varargin{find(tmp)+1}) = varargin{find(tmp)+1};
+%                 input(input<=-varargin{find(tmp)+1}) = -varargin{find(tmp)+1};
+%             end
+            input = input + obj.sigma^2*randn(size(input));
+        end
+        
+        function input = determistic_policy(obj, state)
+            [~,~,c,d] = obj.apx_function.get_ss(obj.params);
+            input = c*obj.internal_state + d*state;
+        end
+        
+        function grad = policy_grad_mu(obj, pre_input, state)
+            [~,~,c,d] = obj.apx_function.get_ss(obj.params);
+            [Abig, Bbig, Cbig, Dbig] = obj.get_sys_big(obj);
+            ABCD = [blkdiag(Abig{:}), blkdiag(Bbig{:}); blkdiag(Cbig{:}). blkdiag(Dbig{:})];
+            xy = ABCD*[obj.grad_internal_state; state];
+            obj.grad_internal_state_ = obj.grad_internal_state;
+            obj.grad_internal_state  = xy(1 : length(obj.grad_internal_state));
+            apx_function_grad = xy(length(obj.grad_internal_state)+1 :  end);
+            grad = ((pre_input - (c*obj.internal_state_ + d*state))./(obj.sigma^2))*apx_function_grad;            
+        end
+        
+        function set_policy_sigma(obj, theta2)
+            obj.sigma = theta2;
+        end
+        
+        function sigma = get_policy_sigma(obj)
+            sigma = obj.sigma;
+        end
+        
+        function iitialize_memory(obj)
+            obj.internal_state  = zeros(size(obj.internal_state));
+            obj.internal_state_ = zeros(size(obj.internal_state_));
+            obj.grad_internal_state  = zeros(size(obj.grad_internal_state));
+            obj.grad_internal_state_ = zeros(size(obj.grad_internal_state_));
+        end
+        
+        function [Abig, Bbig, Cbig, Dbig] = get_sys_big(obj, theta)
+            if nargin == 1 || isempty(theta)
+                theta = obj.get_params();
+            end
+            np = length(theta);
+            [A, B, C, D, dA, dB, dC, dD] = obj.apx_function.get_ss(theta);
+            n = size(A, 1);
+            l = size(C, 1);
+            Abig = kron(eye(np+1), A);
+            Bbig = kron(ones(np+1, 1), B*0);
+            Bbig(1:n,:) = B;
+            Cbig = kron(eye(np+1), C);
+            Dbig = kron(ones(np+1,1), D*0);
+            Dbig(1:l, :) = D;
+            for itr  = 1:numel(obj.params)
+                Abig(itr*n+(1:n), 1:n) = dA{itr};
+                Bbig(itr*n+(1:n), :) = dB{itr};
+                Cbig(itr*l+(1:l), 1:n) = dC{itr};
+                Dbig(itr*l+(1:l), :) = dD{itr};
+            end
+        end
+    end
+end
+
