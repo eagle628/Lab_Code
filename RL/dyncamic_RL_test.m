22b2dc677799f57cecc58ece745adb0c8052a7e4
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2019-09-02 19:25:47 +0900

    various

--- RL/dyncamic_RL_test.m
+++ RL/dyncamic_RL_test.m
@@ -4,32 +4,38 @@ close all
 %% define model
 model = pendulum_model(0.5,0.15,9.8,0.05,0.01);
 
+%% belief_N
+belief_N = 2;
 %% define init controller
 sys = ss(model.A, model.B, eye(2), [], model.Ts);
 controller_n = 2;% state
 controller_l = 1;% output
-controller_m = 2;% input
-while true
-    controller = drss(2,1,2);
-    controller.Ts = model.Ts;
-    loop = loopsens(sys, controller);
-    if loop.Stable
-        break;
-    end
-end
+controller_m = belief_N;% input
+controller = drss(controller_n, controller_l, controller_m);
+% while true
+%     controller = drss(2,1,2);
+%     controller.Ts = model.Ts;
+%     loop = loopsens(sys, controller);
+%     if loop.Stable
+%         break;
+%     end
+% end
 
 %% generate apx function for value
-basis_N = 11^2;
-nnn = sqrt(basis_N);
-range = [-2,2];
-width = (range(2)-range(1))/(nnn-1);
-m = (range(1):width:range(2))';
-range = [-4,4];
-width = (range(2)-range(1))/(nnn-1);
-mm = (range(1):width:range(2))';
-mu = [kron(m,ones(nnn,1)),repmat(mm,nnn,1)]; 
-sigma = 0.25*ones(basis_N, 1);
-RBF1 = Radial_Basis_Function(basis_N, mu, sigma);
+% parmeter
+basis_N = 11;
+% generate line
+% basis function RBF
+range = [-1,1];
+width = (range(2)-range(1))/(basis_N-1);
+m = range(1):width:range(2);
+mu = m;
+for itr = 1 : belief_N * model.ny-1
+mu = combvec(mu, m); % col vector combinater function
+end
+mu = mu';
+sigma = 0.25*ones(size(mu, 1), 1);
+RBF1 = Radial_Basis_Function(size(mu, 1), mu, sigma);
 value  =  value_RBF(RBF1);
 
 %% generate apx function for policy
@@ -40,7 +46,8 @@ policy = policy_dynamic(apx_function, pi_sigma);
 
 %% trainer
 Te = 2;
-train = general_actor_critic_with_eligibility_traces_episodic(model, policy, value, Te);
+% train = general_actor_critic_with_eligibility_traces_episodic(model, policy, value, Te);
+train = general_actor_critic_with_eligibility_traces_episodic_on_POMDP(model, policy, value, Te, belief_N);
 mode_parallel = 'off';
 train_seed = 28;
 [x, u_mpc, u_rl, theta_mu_snapshot, theta_sigma_snapshot, w_snapshot, reward_history] = ...
@@ -48,7 +55,8 @@ train_seed = 28;
 
 %% plot
 %%
-x_all = train.sim([0.4;0], [], 'parallel', mode_parallel);
+% x_all = train.sim([0.4;0], [], 'parallel', mode_parallel);
+x_all = train.sim([0.4;0], []);
 
 figure
 plot(train.t, x_all(:,1), 'r')
