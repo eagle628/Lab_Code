4d350e7447423b8475ba2713cb43098ba4e22db8
unknown <inoue@cyb.sc.e.titech.ac.jp>
2019-11-02 15:55:39 +0900

    modify constraint

--- RL_new/optimizer.m
+++ RL_new/optimizer.m
@@ -5,13 +5,18 @@ classdef optimizer < handle
     
     properties
         target
+        constraint_enable
         counter
     end
     
     methods
-        function obj = optimizer(target)
+        function obj = optimizer(target, constraint_enable)
+            if nargin < 2
+                constraint_enable = false;
+            end
             obj.target = target; 
             obj.counter = 0;
+            obj.constraint_enable = constraint_enable;
         end
         
         function initialize(obj)
@@ -24,9 +29,9 @@ classdef optimizer < handle
            obj.target = target; 
         end
         
-        function update = constraint(obj, neq_params, data)
-            update = true;
-            % pass function
+        function update = constraint(obj, new_params, data)
+            new_controller = obj.target.get_controller(new_params);
+            update = model.constraint(new_controller, data);
         end
         
         function optimizer_initialize(obj)
@@ -36,7 +41,7 @@ classdef optimizer < handle
         function opt(obj, data)
             grad = obj.target.grad(data);
             new_params = obj.updator(grad, data);
-            if obj.constraint(new_params, data)
+            if (~obj.constraint_enable) || obj.constraint(new_params, data)
                obj.target.set_params(new_params);
                obj.counter = obj.counter + 1;
             end
