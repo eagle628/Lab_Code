614ce05ca091325a8b8fe59ed45d690a33ba7cd5
eagle628 <naoya.inoue.6.28.1024.eagle@gmail.com>
2019-12-16 22:31:21 +0900

    ???

--- RL_new/Chainer_Deep_Value.m
+++ RL_new/Chainer_Deep_Value.m
@@ -1,12 +1,12 @@
 classdef Chainer_Deep_Value < Value_base
     % apx_function : Chainer optimizer class
-    
+
     properties
         gpu_device
         fixed_apx_function_enable
         fixed_apx_function
     end
-    
+
     methods
         function obj = Chainer_Deep_Value(apx_function, gpu_device, fixed_apx_function_enable)
             if nargin < 3 || isempty(fixed_apx_function_enable)
@@ -24,15 +24,15 @@ classdef Chainer_Deep_Value < Value_base
                 obj.fixed_apx_function = py.copy.deepcopy(obj.apx_function.target);
             end
         end
-        
+
         function params = get_params(obj)
           params = [];
         end
 
         function set_params(obj, params)
           % pass
-        end 
-        
+        end
+
         function out = predict(obj, state, enable_backprop, varargin)
             state = predict_parser(obj, state);
             if obj.fixed_apx_function_enable && ~enable_backprop
@@ -41,7 +41,7 @@ classdef Chainer_Deep_Value < Value_base
             end
             out = obj.apx_function.target.predict(state, enable_backprop);
         end
-        
+
         function grad(obj, data)
             tmp = py.numpy.array(0 ,pyargs('dtype','float32'));
             tmp = tmp.reshape(int64(1),int64(1));
@@ -50,14 +50,14 @@ classdef Chainer_Deep_Value < Value_base
             obj.apx_function.target.cleargrads();
             loss.backward();
         end
-        
+
         function state = predict_parser(obj, state)
             state = py.numpy.array(state',pyargs('dtype','float32'));
             state = py.numpy.vstack(state);% N*1 2-d numpy array
             state = state.T;% batch size Single
             state = py.chainer.dataset.to_device(obj.gpu_device, state);
         end
-        
+
         function fixed_apx_function_update(obj)
             if obj.fixed_apx_function_enable
                 obj.fixed_apx_function = py.copy.deepcopy(obj.apx_function.target);
